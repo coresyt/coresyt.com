98db8d025c1ff284cf52a8f5c2165f23080a515e
coresyt <corejsyt2011@gmail.com>
2024-11-22 13:35:55 -0600

    refactor(Root): Format and lintings files

--- src/lib/repos.ts
+++ src/lib/repos.ts
@@ -1,63 +1,55 @@
-import type { GithubAPi, GithubAPiRes } from "../types";
+import type { GithubAPi, GithubAPiRes } from "../types"
 
-const API_URL = "https://api.github.com";
+const API_URL = "https://api.github.com"
 
 export async function GetReposGit(): Promise<Array<GithubAPiRes>> {
-  const res = await fetch(`${API_URL}/users/coresyt/repos`, {
-    headers: {
-      Authorization: import.meta.env.GITHUB_TOKEN,
-    },
-  });
-  const json: Promise<Array<GithubAPi>> = await res.json();
-  return (await json)
-    .map((obj: GithubAPi) => {
-      const {
-        name,
-        html_url,
-        language,
-        homepage,
-        stargazers_count,
-        description,
-      } = obj;
-      if (homepage)
-        return {
-          name,
-          html_url,
-          language,
-          page: homepage,
-          stars: stargazers_count,
-          desc: description,
-        };
-      return {
-        name,
-        html_url,
-        language,
-        stars: stargazers_count,
-        desc: description,
-      };
-    })
-    .filter((obj) => obj.name !== "coresyt");
+	const res = await fetch(`${API_URL}/users/coresyt/repos`, {
+		headers: {
+			Authorization: import.meta.env.GITHUB_TOKEN,
+		},
+	})
+	const json: Promise<Array<GithubAPi>> = await res.json()
+	return (await json)
+		.map((obj: GithubAPi) => {
+			const { name, html_url, language, homepage, stargazers_count, description } = obj
+			if (homepage)
+				return {
+					name,
+					html_url,
+					language,
+					page: homepage,
+					stars: stargazers_count,
+					desc: description,
+				}
+			return {
+				name,
+				html_url,
+				language,
+				stars: stargazers_count,
+				desc: description,
+			}
+		})
+		.filter((obj) => obj.name !== "coresyt")
 }
 
 export async function GetRepoGit(name: string): Promise<GithubAPiRes> {
-  const res = await fetch(`${API_URL}/repos/coresyt/add-items-react`);
-  const json: Promise<GithubAPi> = await res.json();
-  const { html_url, language, homepage, stargazers_count, description } =
-    await json;
-  if (homepage)
-    return {
-      name,
-      html_url,
-      language,
-      page: homepage,
-      stars: stargazers_count,
-      desc: description,
-    };
-  return {
-    name,
-    html_url,
-    language,
-    stars: stargazers_count,
-    desc: description,
-  };
+	const res = await fetch(`${API_URL}/repos/coresyt/add-items-react`)
+	const json: Promise<GithubAPi> = await res.json()
+	const { html_url, language, homepage, stargazers_count, description } = await json
+	if (homepage)
+		return {
+			name,
+			html_url,
+			language,
+			page: homepage,
+			stars: stargazers_count,
+			desc: description,
+		}
+	return {
+		name,
+		html_url,
+		language,
+		stars: stargazers_count,
+		desc: description,
+	}
 }
--- src/pages/api/repos.ts
+++ src/pages/api/repos.ts
@@ -1,8 +1,8 @@
-import { GetReposGit } from "@/lib/repos";
-import type { APIRoute } from "astro";
+import { GetReposGit } from "@/lib/repos"
+import type { APIRoute } from "astro"
 
 export const GET: APIRoute = async () => {
-  const repos = await GetReposGit();
+	const repos = await GetReposGit()
 
-  return new Response(JSON.stringify(repos));
-};
+	return new Response(JSON.stringify(repos))
+}
